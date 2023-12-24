dbec84c11afe4167b67f02aef0bceb6ab84bc0c6
Evgeny <jack9807@yandex.ru>
2023-12-24 17:40:09 +0600

    Classwork task 2_4

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,14 @@
+﻿// 4. Напишите программу, которая выводит третью с конца
+// цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Enter number");
+int a = int.Parse(Console.ReadLine()!);
+
+if (a < 100)
+{
+    Console.WriteLine("There's no third number");
+} 
+else
+{
+    Console.WriteLine(a / 100 % 10);    
+}
