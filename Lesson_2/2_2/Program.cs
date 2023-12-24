f24cfe257eb13ac61792bac9c9548bb856c92f78
Evgeny <jack9807@yandex.ru>
2023-12-24 16:51:29 +0600

    Classwork task 2_2

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// 2. Напишите программу, которая принимает на вход трёхзначное
+// число и возводит вторую цифру этого числа в степень, равную
+// третьей цифре. 123
+
+Console.WriteLine("Enter Number");
+int a = int.Parse(Console.ReadLine()!);
+int SecondNumber = a / 10 % 10;
+int ThirdNumber = a % 10;
+Console.WriteLine(Math.Pow(SecondNumber,ThirdNumber));
