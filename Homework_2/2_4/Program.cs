9b60e85455f90d9585a250eb093ab029772ea26f
Evgeny <jack9807@yandex.ru>
2024-01-05 01:13:41 +0600

    Done Homework_2_3

---
+++ Homework_2/2_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает 
+// натуральное число N, а на выходе показывает его цифры через запятую. 12345
+
+Console.WriteLine("Enter number");
+int Number = int.Parse(Console.ReadLine()!);
+int i = 0;
+
+while (Number < 0 )
+{
+    i = Number % 10;
+    Number = Number / -10;
+    Console.Write(i + ",");
+}
