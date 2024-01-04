ad1153a1182b66bf76686ac757fed4557233d8d4
Evgeny <jack9807@yandex.ru>
2024-01-05 01:16:11 +0600

    Premade Homework_2_4

--- Homework_2/2_4/Program.cs
+++ Homework_2/2_4/Program.cs
@@ -2,12 +2,12 @@
 // натуральное число N, а на выходе показывает его цифры через запятую. 12345
 
 Console.WriteLine("Enter number");
-int Number = int.Parse(Console.ReadLine()!);
+int Number = int.Parse(string)(Console.ReadLine()!);
 int i = 0;
 
-while (Number < 0 )
+while (Number > 0 )
 {
     i = Number % 10;
-    Number = Number / -10;
-    Console.Write(i + ",");
+    Number = Number / 10;
+    Console.Write(i + " ");
 }
