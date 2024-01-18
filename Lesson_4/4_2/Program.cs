7247023cee9b5830a3a3f7390e349f511c29172b
Evgeny <jack9807@yandex.ru>
2024-01-18 21:56:45 +0600

    added classwork_4_2_fixed

--- Lesson_4/4_2/Program.cs
+++ Lesson_4/4_2/Program.cs
@@ -22,6 +22,20 @@ int[] MassNums(int size, int from, int to)
         arr[i] = new Random().Next(from, to + 1);
     return arr;
 }
+
+static int CountEvenItems(int[] arr)
+{
+    int count = 0;
+    foreach (int number in arr)
+    {
+        if (number % 2 == 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
 Console.WriteLine("Задайте желаемое количество чисел в массиве");
 int num = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Задайте минимальное число");
@@ -31,3 +45,5 @@ int stop = int.Parse(Console.ReadLine()!);
 
 int[] mass = MassNums(num, start, stop);
 Print(mass);
+Console.WriteLine(CountEvenItems(mass));
+
