a1424e187bb79c6d4e3255d7be02383819b4172f
Evgeny <jack9807@yandex.ru>
2024-02-02 22:42:55 +0600

    added lesson_5_3

---
+++ Lesson_5/5_3/Program.cs
@@ -0,0 +1,71 @@
+﻿// Задайте двумерный массив из целых чисел. Сформируйте новый
+// одномерный массив, состоящий из средних арифметических
+// значений по строкам двумерного массива.
+
+void PrintDouble(double[] arr)
+{
+int size = arr.Length;
+
+for (int i = 0; i < size; i++)
+Console.Write($"{arr[i]} ");
+
+Console.WriteLine();
+}
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j]} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    }
+    return arr;
+}
+
+double[] AverageSum(int[,] array)
+{
+    double sum = 0;
+    double[] newMass = new double[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            sum += array[i, j];
+        }
+        newMass[i] = sum/array.GetLength(1);
+        sum = 0;
+    }
+    return newMass;
+}
+
+Console.Write("Enter the number of rows: ");
+int row_num = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int column_num = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(row_num, column_num, start, stop);
+
+
+Print(mass);
+PrintDouble(AverageSum(mass));
