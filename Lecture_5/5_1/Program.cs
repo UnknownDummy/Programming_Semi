58dd229e58efe2c5741530f585ea5034ce4efcbf
Evgeny <jack9807@yandex.ru>
2024-02-02 20:30:01 +0600

    added lecture_5_1

---
+++ Lecture_5/5_1/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Вычислить факториал от натурального числа N
+*/
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+    {
+        Console.WriteLine($"Stop: {n}");
+        return 1;
+    }
+    Console.WriteLine(n);
+    return n * Fact(n - 1);
+}
+
+Console.Write(Fact(5));
