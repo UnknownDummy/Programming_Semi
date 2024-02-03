5146eac21358e9caa83af7f2bc7cac93d0bd415a
Evgeny <jack9807@yandex.ru>
2024-02-04 01:31:58 +0600

    added Homework_6_3

---
+++ Homework_6/6_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+class Program
+{
+    static void StrRevrs(string strInput)
+    {
+        string[] TempArray = strInput.Split(' '); // создаём рабочий массив и вводим разделитель
+        Array.Reverse(TempArray); // разворачиваем массив с помощью метода Reverse
+        string reverse = string.Join(" ", TempArray); // выводим в результат в строку
+        Console.Write($"{reverse}"); // выводим итог на экран
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите строку, разделённую пробелами: ");
+        string strUsrInput = Console.ReadLine()!;
+        StrRevrs(strUsrInput);
+    }
+}
