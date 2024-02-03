485de2b9ff4a08a39ac570126c6978e519231665
Evgeny <jack9807@yandex.ru>
2024-02-03 23:23:42 +0600

    Classwork_6_3

---
+++ Lesson_6/6_3/Program.cs
@@ -0,0 +1,33 @@
+﻿// 3. Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+
+using System;
+
+class Program
+{
+    private static int CountVowels(string input) // Метод для подсчета количества гласных букв в строке
+    {
+        char[] vowels = { 'a', 'e', 'i', 'o', 'u' }; // Создаем массив с гласными буквами
+
+        int count = 0;
+        for (int i = 0; i < input.Length; i++) // Проходим по каждому символу во введенной строке
+        {
+            if (Array.IndexOf(vowels, input[i]) != -1) // Если символ является гласной, увеличиваем счетчик
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Введите строку из латинских букв в нижнем регистре:"); // Просим пользователя ввести строку
+        string input = Console.ReadLine()!;
+
+        int vowelCount = CountVowels(input);
+ 
+        Console.WriteLine($"Количество гласных букв в строке: {vowelCount}"); // Выводим результат
+    }
+}
