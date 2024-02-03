fb6bbb8cd8e794d5dc3b2c4dc9b83af46017b304
Evgeny <jack9807@yandex.ru>
2024-02-04 01:08:11 +0600

    added Homework_6_3

---
+++ Homework_6/6_3/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте произвольную строку. Выясните,
+// является ли она палиндромом.
+
+using System;
+
+class Program
+{
+    static void PalindromeChk(string strInput)
+    {
+        for (int i = 0; i < strInput.Length/2; i++)
+        {
+            if (strInput[i] != strInput[strInput.Length - 1 - i])
+            {
+                Console.Write($"Строка {strInput} не является палиндромом.");
+                return;
+            }
+        
+        }
+        Console.Write($"Строка {strInput} является палиндромом.");
+        return;
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите произвольную строку: ");
+        string strUsrInput = Console.ReadLine()!;
+        Console.WriteLine("Проверка на палиндромность...");
+        PalindromeChk(strUsrInput);
+    }
+}
