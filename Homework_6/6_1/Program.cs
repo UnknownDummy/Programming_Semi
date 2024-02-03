dc4275225f6b88af7dad5f851dc502188d28ae4e
Evgeny <jack9807@yandex.ru>
2024-02-04 00:23:33 +0600

    added Homework_6_1

---
+++ Homework_6/6_1/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте двумерный массив символов (тип char [,]).
+// Создать строку из символов этого массива. 
+
+using System;
+
+class Program
+{
+    static string ArrToStr(char[,] arr)
+    {
+        int row_size = arr.GetLength(0);
+        int column_size = arr.GetLength(1);
+        string strOutput = "";
+
+        for (int i = 0; i < row_size; i++)
+        {
+            for (int j = 0; j < column_size; j++)
+                strOutput += arr[i, j];
+        }
+        return strOutput;
+    }
+
+    static void Main()
+    {
+        char[,] charArr = {
+            {'a', 'b', 'c'},
+            {'d', 'e', 'f'}
+        };
+        Console.Write(ArrToStr(charArr));
+    }
+}
