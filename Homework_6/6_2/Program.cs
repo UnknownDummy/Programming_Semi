ffd9bd239ecf75addf2320c3d7a5b3e7ac827acc
Evgeny <jack9807@yandex.ru>
2024-02-04 00:41:06 +0600

    added Homework_6_2

---
+++ Homework_6/6_2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте строку, содержащую латинские буквы
+// в обоих регистрах. Сформируйте строку, в которой все
+// заглавные буквы заменены на строчные.
+
+using System;
+
+class Program
+{
+    static string lowerString(string strInput)
+    {
+        string strOutput = strInput.ToLower();
+        return strOutput;    
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите строку, содержащую латинские буквы в обоих регистрах: ");
+        string strUsrInput = Console.ReadLine()!;
+        Console.Write(lowerString(strUsrInput));
+    }
+}
