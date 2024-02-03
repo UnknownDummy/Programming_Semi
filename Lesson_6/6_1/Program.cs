00a3913bd06f4c59effc37f85ab9947da0ff2d00
Evgeny <jack9807@yandex.ru>
2024-02-03 21:21:40 +0600

    Classwork_6_1

---
+++ Lesson_6/6_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1. Задайте массив символов (тип char []).
+// Создайте строку из символов этого массива.
+
+// Конструктор строки вида string(char []) не использовать.
+
+string concat(char[] mass)
+{
+    string buf = "";
+    for (int i = 0; i < mass.Length; i++)
+    {
+        buf += mass[i];
+
+    }
+    return buf;
+}
+
+char[] charArr = { 'H', 'e', 'l', 'l', 'o' };
+string res = concat(charArr);
+Console.WriteLine(res);
