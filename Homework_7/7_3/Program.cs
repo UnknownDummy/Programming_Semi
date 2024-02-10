4443e13d6cf048ed4d9591eb680c21f784ae9291
Evgeny <jack9807@yandex.ru>
2024-02-11 01:13:06 +0600

    added Homework_7_3

---
+++ Homework_7/7_3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете
+// его элементы, начиная с конца. Использовать
+// рекурсию, не использовать циклы.
+
+void printReverseMass(int[] numbers, int i)
+{
+    if (i < 0) return;
+    Console.Write(numbers[i] + " ");
+    printReverseMass(numbers, i - 1);
+}
+
+int[] massNums = { 1, 2, 5, 0, 10, 34 };
+int NumsLength = massNums.Length - 1;
+printReverseMass(massNums, NumsLength);
