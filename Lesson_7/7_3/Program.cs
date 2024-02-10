b00bb9344e493853e53baaa98b49fc510e9813e9
Evgeny <jack9807@yandex.ru>
2024-02-10 23:35:49 +0600

    Classwork_7_3

---
+++ Lesson_7/7_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// 3. Считать строку с консоли, содержащую латинские буквы.
+// Вывести на экран согласные буквы этой строки.
+//
+// Использовать рекурсию. Не использовать цикл
+
+string str1 = "aeyiou";
+
+void printCons(string str, int i = 0)
+{
+    if (i == str.Length) return;
+    if (str1.Contains(char.ToLower(str[i])) == false)
+    Console.Write(str[i] + " ");
+    printCons(str, i + 1);
+}
+
+Console.Write("Введите строку для рекурсии:");
+string str2 = Console.ReadLine()!;
+printCons(str2);
