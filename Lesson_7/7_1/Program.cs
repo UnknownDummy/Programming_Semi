ff9bd40eaade1b38ae7d6ba4607cbd96fb4ef606
Evgeny <jack9807@yandex.ru>
2024-02-10 22:58:53 +0600

    Classwork_7_1

---
+++ Lesson_7/7_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// 1. Напишите программу, которая будет принимать на вход число
+// и возвращать сумму его цифр. Использовать рекурсию.
+
+int Recursion(int a)
+{
+    if (a == 0)
+    {
+        return 0;
+    }
+    return Recursion(a / 10) + a %  10;
+}
+
+Console.Write("Введите число для рекурсии:");
+int output = int.Parse(Console.ReadLine()!);
+Console.Write($"{Recursion(output)}");
