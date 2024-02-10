0f65288075e80742922943172c207a4370e29485
Evgeny <jack9807@yandex.ru>
2024-02-11 00:11:50 +0600

    added Homework_7_1

---
+++ Homework_7/7_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Задайте значения M и N. Напишите
+// программу, которая выведет все натуральные числа
+// в промежутке от M до N. Использовать рекурсию, не
+// использовать циклы.
+
+void Recursion(int MinNumber, int MaxNumber)
+{
+    if (MaxNumber >= MinNumber)
+    {
+        Recursion(MinNumber, MaxNumber - 1);
+        Console.Write($"{MaxNumber} ");
+    }
+}
+
+Console.Write("Введите начальное число для рекурсии: ");
+int inputMin = int.Parse(Console.ReadLine()!);
+Console.Write("Введите конечное число для рекурсии: ");
+int inputMax = int.Parse(Console.ReadLine()!);
+Recursion(inputMin, inputMax);
