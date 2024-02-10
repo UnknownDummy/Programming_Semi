187bb62dd185d035052faf1d0f3499187cb748de
Evgeny <jack9807@yandex.ru>
2024-02-10 23:07:34 +0600

    Classwork_7_2

---
+++ Lesson_7/7_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2. Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+
+void Recursion(int a)
+{
+    if (a != 0)
+    {
+        Recursion(a - 1);
+        Console.Write($"{a} ");
+    }
+}
+
+Console.Write("Введите число для рекурсии:");
+int output = int.Parse(Console.ReadLine()!);
+Recursion(output);
